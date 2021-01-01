50a7e32e550ff7d61fb378337b2f536f59ccacc4
amanda <amanda@pop-os.localdomain>
2021-01-01 01:21:47 -0500

    customised stuff

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,27 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'John Doe';
+var name = 'Amanda';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
+var gree1 = 'It\'s past your bedtime!';
+var gree2 = 'Mornin\' ';
+var gree3 = 'Afternoon, ';
+var gree4 = 'Good evening, ';
 var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
+// Punctuation
+var ep = '!';
+var peri = '.';
+
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-    document.getElementById('greetings').innerText = gree1 + name;
+    document.getElementById('greetings').innerText = gree1;
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2 + name;
+    document.getElementById('greetings').innerText = gree2 + name + ep;
 } else if (hour >= 12 && hour < 17) {
-    document.getElementById('greetings').innerText = gree3 + name;
+    document.getElementById('greetings').innerText = gree3 + name + ep;
 } else  {
-    document.getElementById('greetings').innerText = gree4 + name;
+    document.getElementById('greetings').innerText = gree4 + name + peri;
 }
--- js/weather.js
+++ js/weather.js
@@ -5,15 +5,15 @@ const descElement = document.querySelector('.temperature-description p');
 // App data
 const weather = {};
 weather.temperature = {
-    unit: 'celsius',
+    unit: 'fahrenheit',
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d25';
+const key = '6ad2f2ab266b9a5e1b03e82e64828d68';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 33.736789;
+    let longitude = -84.399827;
 
     getWeather(latitude, longitude);
 }
