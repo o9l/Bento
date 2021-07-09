84f5591962e313d21df55e8461a967bcd27e559d
ArsonW <amanda1usernamesarehard@protonmail.com>
2021-07-09 03:51:43 -0400

    Changed name.

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'Amanda';
+var name = 'Arson';
 
 // Here you can change your greetings
 var gree1 = 'It\'s past your bedtime!';
