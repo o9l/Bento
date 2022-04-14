4739c25c976108a02a0f91cc091403f2550aabfd
o9l <76831677+o9l@users.noreply.github.com>
2022-04-13 20:10:01 -0400

    changed name

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'Arson';
+var name = 'Matcha';
 
 // Here you can change your greetings
 var gree1 = 'It\'s past your bedtime!';
